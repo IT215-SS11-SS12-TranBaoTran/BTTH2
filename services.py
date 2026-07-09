e98c7b3b7ecc63f4f33dc97b618694e3dc90752a
Baotran07-zip <bao.tran.073108@gmail.com>
2026-07-09 07:24:41 +0700

    Update services.py

--- services.py
+++ services.py
@@ -1,14 +1,53 @@
 from sqlalchemy.orm import Session
-from fastapi import HTTPException, status
-from models import OrderModel
+from fastapi import HTTPException
+from models import SmartHomePlan
 
-def get_order_by_id_service(db: Session, order_id: int):
-    order = db.query(OrderModel).filter(OrderModel.id == order_id).first()
+def create_plan(db: Session, plan):
 
-    if order is None:
+    check = db.query(SmartHomePlan).filter(
+        SmartHomePlan.plan_code == plan.plan_code
+    ).first()
+
+    if check:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Order not found"
+            status_code=400,
+            detail="Plan code already exists"
         )
-        
-    return order
+
+    try:
+        new_plan = SmartHomePlan(
+            plan_code=plan.plan_code,
+            plan_name=plan.plan_name,
+            device_quantity=plan.device_quantity,
+            price=plan.price
+        )
+
+        db.add(new_plan)
+        db.commit()
+        db.refresh(new_plan)
+
+        return new_plan
+
+    except:
+        db.rollback()
+        raise HTTPException(
+            status_code=500,
+            detail="Database Error"
+        )
+
+def get_all_plans(db: Session):
+    return db.query(SmartHomePlan).all()
+
+def get_plan_by_id(db: Session, plan_id: int):
+
+    plan = db.query(SmartHomePlan).filter(
+        SmartHomePlan.id == plan_id
+    ).first()
+
+    if not plan:
+        raise HTTPException(
+            status_code=404,
+            detail="Plan not found"
+        )
+
+    return plan
