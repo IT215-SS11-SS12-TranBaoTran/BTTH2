973c1a47de509008d05a6a980c7033f1d9285e55
Baotran07-zip <bao.tran.073108@gmail.com>
2026-07-09 07:23:44 +0700

    Update database connection details and structure

--- database.py
+++ database.py
@@ -1,15 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:password@localhost:3306/ecommerce_db"
+DATABASE_URL = "mysql+pymysql://root:123456@localhost/smart_home"
 
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 Base = declarative_base()
 
