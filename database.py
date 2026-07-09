29658394a99148df541c76972f5a61ad54dab598
Baotran07-zip <bao.tran.073108@gmail.com>
2026-07-09 07:25:00 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:123456@localhost/smart_home"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:password@localhost:3306/smart_home_db"
 
-engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
